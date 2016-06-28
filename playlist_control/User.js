351c45999be5ef49a4565663988367e2846a1175
lazamar <lazaronijunior@gmail.com>
2016-06-28 16:31:48 +0100

    Add setTracks method to user

--- playlist_control/User.js
+++ playlist_control/User.js
@@ -50,6 +50,17 @@ module.exports = class User {
   }
 
   /**
+   * Replaces all user tracks for new ones.
+   * @public
+   * @method setTracks
+   * @param  {[type]} tracks [description]
+   */
+  setTracks(tracks) {
+    this.tracks = [];
+    tracks.forEach((t) => this.addTrack(t));
+  }
+
+  /**
    * Returns the track in the index requested and removes it from
    * the tracks array
    * @method extractTrack
@@ -66,26 +77,26 @@ module.exports = class User {
     return track;
   }
 
-  /**
-   * @public
-   * @method setTrackOrder
-   * @throws if trackOrder doesn't have the exact same tracks as user.tracks.
-   * @param  {Array<Object>} tracks - Array of track objects. Must contain the
-   * same track ids that the user's current tracks array has.
-   */
-  setTrackOrder(tracks) {
-    assert(Array.isArray(tracks), `Invalid tracks parameter. ${tracks} is not an array.`);
-    assert(tracks.length === this.tracks.length, 'Incompatible tracklist size in reordering.');
-    const sorted = Array.from(this.tracks);
-    sorted.sort((t1, t2) => {
-      const t1NewIndex = tracks.findIndex(t => t.id === t1.getId());
-      const t2NewIndex = tracks.findIndex(t => t.id === t2.getId());
-      assert(t1NewIndex !== -1, `User track not in reordering array: ${t1.getInfo().name}`);
-      assert(t2NewIndex !== -1, `User track not in reordering array: ${t2.getInfo().name}`);
-      return t1NewIndex - t2NewIndex;
-    });
-    this.tracks = sorted;
-  }
+  // /**
+  //  * @public
+  //  * @method setTrackOrder
+  //  * @throws if trackOrder doesn't have the exact same tracks as user.tracks.
+  //  * @param  {Array<Object>} tracks - Array of track objects. Must contain the
+  //  * same track ids that the user's current tracks array has.
+  //  */
+  // setTrackOrder(tracks) {
+  //   assert(Array.isArray(tracks), `Invalid tracks parameter. ${tracks} is not an array.`);
+  //   assert(tracks.length === this.tracks.length, 'Incompatible tracklist size in reordering.');
+  //   const sorted = Array.from(this.tracks);
+  //   sorted.sort((t1, t2) => {
+  //     const t1NewIndex = tracks.findIndex(t => t.id === t1.getId());
+  //     const t2NewIndex = tracks.findIndex(t => t.id === t2.getId());
+  //     assert(t1NewIndex !== -1, `User track not in reordering array: ${t1.getInfo().name}`);
+  //     assert(t2NewIndex !== -1, `User track not in reordering array: ${t2.getInfo().name}`);
+  //     return t1NewIndex - t2NewIndex;
+  //   });
+  //   this.tracks = sorted;
+  // }
 
   /**
    * @public
