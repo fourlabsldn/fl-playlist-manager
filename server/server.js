7faa73f1dbec01c6e3299b6a0449ebb0953749cb
lazamar <lazaronijunior@gmail.com>
2016-06-28 16:22:17 +0100

    Add post body parser

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 /* eslint-env node */
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
 
 module.exports = (spotify, roundHandler) => {
+  const app = express();
+  app.use(bodyParser.json()); // to support JSON-encoded bodies
+  app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
   app.get('/getTrackList', (req, res) => {
     const trackList = roundHandler.getTrackList();
     res.json(trackList);
+  });
+
+  app.post('/setUserTracks', (req, res) => {
+    const tracks = req.body.tracks;
+    const user = req.body.user;
+    console.log('Tracks: ', tracks);
+    console.log('User:', user);
+    res.json({ success: true });
   });
 
   app.get('/', (req, res) => {
