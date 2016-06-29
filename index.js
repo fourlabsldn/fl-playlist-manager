613977e079fc0b361dd2155d080f3b2fdc1f442e
lazamar <lazaronijunior@gmail.com>
2016-06-29 16:03:18 +0100

    Change playlist name for testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
 const PlaylistController = require('./server/PlaylistController');
-const playlistName = 'Office';
+const playlistName = 'Test-Office'; // Remember to change this for production
 const playlistController = new PlaylistController(playlistName);
 
 const server = require('./server/server');
