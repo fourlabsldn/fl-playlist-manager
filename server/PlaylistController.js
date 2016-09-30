1917762755a345c2f0841608e364ae9e463f1fbb
lazamar <lazaronijunior@gmail.com>
2016-09-30 10:18:26 +0100

    Fix DOS problem

--- server/PlaylistController.js
+++ server/PlaylistController.js
@@ -59,7 +59,7 @@ module.exports = class PlaylistController {
       this.spotify.addTracks(trackUris);
     } catch (e) {
       this.spotify.refreshAccessToken();
-      console.error('Error: ', e);
+      console.error('Error at submitToSpotify: ', e);
     }
 
     // Get the latest time.
@@ -73,7 +73,7 @@ module.exports = class PlaylistController {
     const timeToPLaylistEnd = this.playlistFinishTime.diff(moment(), 'milliseconds'); // in ms
     // submit next track around 10 seconds before the tracklist ends.
     // guarantee a minimum time of 10 seconds between submissions. To avoid a DOS attack on our router.
-    const timeToNextSubmission = Math.min(timeToPLaylistEnd - 10000, 10000); // in ms
+    const timeToNextSubmission = Math.max(timeToPLaylistEnd - 10000, 10000); // in ms
 
     // Schedule next update
     setTimeout(() => this.submitToSpotify(), timeToNextSubmission);
